1f847f0e06f86c7f96435e69be382cdadcc125e3
Brelio <julbrehin@gamil.com>
2015-11-18 13:28:08 +0100

    Moved steering tests into one folder + created steering step response test

--- tests/steeringTests/steeringStepResponse/arduino/steeringStepResponse.ino
+++ tests/steeringTests/steeringStepResponse/arduino/steeringStepResponse.ino
@@ -3,6 +3,10 @@
 #include "motor_driver.h"
 #include "myservo.h"
 
+// This is the value to change to change the steering angle
+// (servo pulse-width)
+#define SERVO_PW 1590
+
 struct k_t *pTaskInfo, *task2;
 
 char stack[300];
@@ -14,18 +18,12 @@ char byte1;
 char byte2;
 int batReading;
 
-void P_controller(){
+void steeringStepResponse(){
 
   const float Wantedspeed = 2;
   float Speedtoduty;
+  int servoPulseWidth = SERVO_MIDDLE_PW; // 1578s pulse width makes the vehicle go straight(-ish)
 
-  float Actualspeed;
-  float Error;
-  const float PGain = 1.0;
-  const float SysGain = 0.49;
-  int duty = 20;
-  float test;
-  float feedFwd = Wantedspeed;
   while(1){
     
     batReading = analogRead(8); //reading battery voltage
@@ -33,62 +31,68 @@ void P_controller(){
     speed1 = getSpeed(1);       //reading speed of the other belt
     timestamp = millis();       //getting time at which data was recorded
 
-
     Actualspeed = (speed0 + speed1)/2; // average speed of the vehicle
+
+    // The servo is set whatever happens
+    setServo(pulsewidth);
+
+    // Vehicle starts after 4 seconds
     if(timestamp>4000){
-    Error = Wantedspeed - Actualspeed;
-    //duty = Wantedspeed*Speedtoduty*100;
-    Speedtoduty = 1.0/(((float)batReading/102.4)*SysGain);// Battery reading: 1024 = 10V, so 1V = 102.4. multiply that with the system gain to calculate the duty cycle.
-    test = (((Error)*PGain+feedFwd+0.38)*Speedtoduty)*100.0; 
-    duty = test;
+    // Battery reading: 1024 = 10V, so 1V = 102.4. multiply that with the system gain to calculate the speed-to-duty-cycle ratio.
+    Speedtoduty = 1.0/(((float)batReadSpeeing/102.4)*SysGain) * 100.0;
+    duty = Wantedspeed*Speedtoduty;
+    // The motor duty cycle should stay in the range of 0-100%
     if(duty > 100) duty = 100;
     if(duty < 0) duty = 0;
     }
-    //number between 0 and a 100%
+    // Steering is triggered 3s later (at t=7s)
+    if(timestamp>7000) servoPulseWidth = SERVO_PW;
 
     if(timestamp < 10000) speed(duty);
     else speed(0);
     
-    //printing out the data whith commas for easy export as .scv-file:
+    //printing out the data whith commas for easy export as .csv-file:
+    CompassGet();
+    Serial.print(",");
     Serial.print((float)batReading/102.4);
     Serial.print(",");
-    Serial.print(duty);
-    Serial.print(",");
-    Serial.print(Error);
-    Serial.print(",");
-    Serial.print(speed0);
-    Serial.print(",");
-    Serial.print(speed1);
+    Serial.print(Actualspeed);
     Serial.print(",");
     Serial.print(timestamp);
     Serial.print("\n");
-    Serial.print("\r");       //carrige return to return the curser for each new line
+    Serial.print("\r");       //carriage return to return the curser for each new line
     delay(30);
     }
   }
 
 void setup() {
-    k_init(2,1,0);
+  k_init(2,1,0);
 
   pinMode(5,OUTPUT);
   initServo();
-  initMotor();
+  
   pinMode(dPinMove1, OUTPUT); 
   pinMode(dPinMove2, OUTPUT);
   pinMode(dPinOnOff, OUTPUT);
 
+  // Set up the motor
+  initMotor();
   enableMotor(1);
   direction(1);
+  
+  // Set up the compass/gyro/accelerometer sensors
+  CompassSetup();
+
+  // Init Hall sensors
+  pinMode(35,INPUT);
+  initHallTimers();
 
   Serial.begin(9600);
   Serial.println("REBOOT");
-  pinMode(35,INPUT);
-
-  initHallTimers();
 
   delay(2000);
   pTaskInfo=k_crt_task(tSpeed,10,stack,300);
-  task2=k_crt_task(P_controller,11,stack2,300);
+  task2=k_crt_task(steeringStepResponse,11,stack2,300);
 
   k_start(1); // krnl runs with 1 msec heartbeat
   /* NOTE: legal time values:
@@ -99,6 +103,5 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // Do not put code here when using JDN's krnl
 }
