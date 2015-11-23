0194fa5276b0e49dc8e03cf387ae89a8d14e4d1d
TempestSky <mgotth13@student.aau.dk>
2015-11-23 21:43:44 +0100

    Proto

--- Protocol/ProtocolReceive/ProtocolReceive.ino
+++ Protocol/ProtocolReceive/ProtocolReceive.ino
@@ -7,12 +7,14 @@ typedef struct {
   uint32_t Checksum;
 }Protocol;
 
-void DataToStruct(uint8_t Data[11])
+Protocol decript;
+
+void DataToStruct(uint8_t Data[11], int foul)
 {
-  Protocol decript;
+  //Protocol decript;
   
   decript.Source = Data[1];
-  decript.Length = Data[2] << 1;
+  decript.Length = Data[2] & 127;
   
   uint8_t Tempx =  Data[4] << 2;
   uint16_t TempX2 = Tempx;
@@ -87,37 +89,20 @@ void DataToStruct(uint8_t Data[11])
 
   uint32_t TempC = TempC1 + TempC2 + TempC3 + decript.Checksum;
 
-  Serial.println(TempC,BIN);
-
-  Serial.println(TempC1,BIN);
-  Serial.println(TempC2,BIN);
-  Serial.println(TempC3,BIN);
-  Serial.println(decript.Checksum,BIN);
-
   while(TempC > 1048575)
   {
     uint32_t TempAdd = TempC >> 20;
     uint32_t TempRest = TempC & 1048575;
     TempC = TempAdd + TempRest;
   }
-
-  Serial.println(TempC,BIN);
-
-  
 }
 
-
-void setup() {
-  Serial.begin(9600);
-}
-
-void loop() {
-
+int GetProtocol(int out[4])
+{  
   uint8_t Data[11];
   int TempLook = -1;
-  int Done;
   
-  while(TempLook == -1)
+  while(TempLook == -1) //Searching for the 1st byte.
   {
     if (Serial.available() > 0)
     {
@@ -126,45 +111,74 @@ void loop() {
     }
   }
   
+  if(TempLook != 128) //If 1st byte is not source, return foul 1.
+  {
+    return 1;
+  }
+
+  
   TempLook = -1;
-  while(TempLook == -1)
+  while(TempLook == -1) //Searching for the 2nd byte.
   {
     if (Serial.available())
     {
       TempLook = Serial.read();
       Data[1] = TempLook;
-
-      if (Data[1] = 128)
-      {
-        TempLook = -1;
-        while(TempLook == -1)
-        {          
-          if (Serial.available())
-          {
-            TempLook = Serial.read();
-
-            char Temp = TempLook >> 1;
-            Data[2] = TempLook;
-
-            if (Temp = 160)
-            {
-              int BytesRead = 3;
-              while (BytesRead < 11)
-              {
-                if (Serial.available())
-                {
-                  Data[BytesRead] = Serial.read();
-                  BytesRead++;
-                }
-              }
-
-              DataToStruct(Data);
-            }                   
-          }          
-        }        
-      }
     }
   }
+
+  if(TempLook != 128) //If 2nd byte is not source, return foul 1.
+  {
+    return 1;
+  }
+
+
+  TempLook = -1;
+  while(TempLook == -1) //Searching for the 2rd byte.
+  {
+    if (Serial.available())
+    {
+      TempLook = Serial.read();
+      Data[2] = TempLook;
+    }
+  }
+
+  int Counter = Data[2] & 127;
+  Counter = (Counter / 8);
+  int BytesRead = 3;
+  while (BytesRead < Counter)
+  {
+    if (Serial.available())
+    {
+      Data[BytesRead] = Serial.read();
+      BytesRead++;
+    }
+  }
+
+  int foul = 0;
+  DataToStruct(Data, foul);
+  out[0] = decript.Source;
+  out[1] = decript.Xcor;
+  out[2] = decript.Ycor;
+  out[3] = decript.Zcor;
+  return foul; 
+}
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+  int Output[4];
+  int Done = GetProtocol(Output);
+
+  for(int i = 0; i < 4; i++)
+  {
+    Serial.println(Output[i]);
+  }
+  
 }
 
 
