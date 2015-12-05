4faaf34bcc7fff85033c27054362868acda2020f
Aand <julbrehin@gmail.com>
2015-12-05 12:18:51 +0100

    Modified steering gain matlab script

--- tests/steeringTests/steeringStepResponse/matlab/test12stepresponseGain1.5/solveSteeringGain.m
+++ tests/steeringTests/steeringStepResponse/matlab/test12stepresponseGain1.5/solveSteeringGain.m
@@ -49,31 +49,30 @@ ylabel('Angle [°]')
 % xlim([3 7]);
 legend('Theta at 1,1 m\cdot s^{-1}', 'Theta at 1,2 m\cdot s^{-1}', 'Theta at 1,3 m\cdot s^{-1}', 'Theta at 1,4 m\cdot s^{-1}', 'Theta at 1,7 m\cdot s^{-1}','Theta at 1,6 m\cdot s^{-1}')
 grid on
-%set(gca,'GridLineStyle',':', 'GridColor', 'k', 'GridAlpha', .6)
+set(gca,'GridLineStyle',':', 'GridColor', 'k', 'GridAlpha', .6)
 
-% %% Gain calculations
-% syms K
-% 
-% Kp = 2.1;
-% ThetaRef = 0;
-% 
-% % Read the data from the test
-% Data1 = csvread('stepresponse1.2-60.csv');
-% 
-% t     = Data1(:,1);
-% Theta = Data1(:,2);
-% ThetaError = Data1(:,3);
-% 
-% % Plots
-% %P1 = scatter(ThetaError,Theta);
-% % ThetaCalc = ThetaRef*Kp*K*exp(-Kp*K*t)
-% 
-% % Make the plots beautiful
-% % set( P1, 'color', '[0 0 1]', 'LineWidth', 2 );
-% title('Step response of steering')
-% xlabel('Error')
-% ylabel('Angle')
-% % xlim([3 7]);
+%% Gain calculations
+figure
+syms K
+
+Kp = 2.1;
+ThetaRef = 0;
+
+% Read the data from the test
+dat = dlmread('plot test12 - Feuille 1.tsv','\t');
+
+
+% Plots
+P1 = scatter(dat(:,1),dat(:,5))
+% P1 = scatter(ThetaError,Theta);
+% ThetaCalc = ThetaRef*Kp*K*exp(-Kp*K*t)
+
+% Make the plots beautiful
+% set( P1, 'color', '[0 0 1]', 'LineWidth', 2 );
+title('Steering gain')
+ylabel('Steering gain')
+xlabel('Vehicle\'s velocity')
+% xlim([3 7]);
 % legend('Theta error', 'Theta','Velocity of X m \cdot s^{-1}')
-% grid on
-% %set(gca,'GridLineStyle',':', 'GridColor', 'k', 'GridAlpha', .6)
+grid on
+set(gca,'GridLineStyle',':', 'GridColor', 'k', 'GridAlpha', .6)
