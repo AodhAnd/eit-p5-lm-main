6cbd9ff21b740e36eb2fbf805a2e45a465e5854f
romaric <romaric.destremau@gmail.com>
2015-12-06 16:47:52 +0100

    corrected the script

--- tests/steeringTests/steeringStepResponse/matlab/test12stepresponseGain1.5/solveSteeringGain.m
+++ tests/steeringTests/steeringStepResponse/matlab/test12stepresponseGain1.5/solveSteeringGain.m
@@ -47,32 +47,40 @@ title('Step response of steering')
 xlabel('Time [ms]')
 ylabel('Angle [°]')
 % xlim([3 7]);
-legend('Theta at 1,1 m\cdot s^{-1}', 'Theta at 1,2 m\cdot s^{-1}', 'Theta at 1,3 m\cdot s^{-1}', 'Theta at 1,4 m\cdot s^{-1}', 'Theta at 1,7 m\cdot s^{-1}','Theta at 1,6 m\cdot s^{-1}')
+legend('Theta at 1,1 m\cdot s^{-1}', 'Theta at 1,2 m\cdot s^{-1}', 'Theta at 1,3 m\cdot s^{-1}', 'Theta at 1,4 m\cdot s^{-1}', 'Theta at 1,7 m\cdot s^{-1}','Theta at 1,8 m\cdot s^{-1}')
 grid on
-set(gca,'GridLineStyle',':', 'GridColor', 'k', 'GridAlpha', .6)
+%set(gca,'GridLineStyle',':', 'GridColor', 'k', 'GridAlpha', .6)
 
 %% Gain calculations
 figure
 syms K
 
-Kp = 2.1;
+Kp = 1.5;
 ThetaRef = 0;
 
 % Read the data from the test
-dat = dlmread('plot test12 - Feuille 1.tsv','\t');
+dat = dlmread('gainCalc.tsv','\t');
 
 
 % Plots
+hold on
 P1 = scatter(dat(:,1),dat(:,5))
 % P1 = scatter(ThetaError,Theta);
 % ThetaCalc = ThetaRef*Kp*K*exp(-Kp*K*t)
+
+P = polyfit(dat(:,1),dat(:,5),1)
+x = [0:0.01:2.5]
+y = 0.4880 * x-0.2173
+% le point avant le chapeau est très important !!
+
+plot(x,y);
 
 % Make the plots beautiful
 % set( P1, 'color', '[0 0 1]', 'LineWidth', 2 );
 title('Steering gain')
 ylabel('Steering gain')
-xlabel('Vehicle\'s velocity')
+xlabel('Vehicle velocity')
 % xlim([3 7]);
 % legend('Theta error', 'Theta','Velocity of X m \cdot s^{-1}')
 grid on
-set(gca,'GridLineStyle',':', 'GridColor', 'k', 'GridAlpha', .6)
+%set(gca,'GridLineStyle',':', 'GridColor', 'k', 'GridAlpha', .6)
