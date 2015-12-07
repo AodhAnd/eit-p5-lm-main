d2af8260ee78e387eb1bd96d1d0989b91370f8b8
Amalie <apet13@student.aau.dk>
2015-12-07 16:19:24 +0100

    filter

--- tests/steeringTests/steeringStepResponse/matlab/test12stepresponseGain1.5/gainCalculation.m
+++ tests/steeringTests/steeringStepResponse/matlab/test12stepresponseGain1.5/gainCalculation.m
@@ -17,16 +17,17 @@ P1 = scatter(dat(:,2),dat(:,5))
 
 P = polyfit(dat(:,2),dat(:,5),1)
 x = [0:0.01:2.5];
-y = 0.4880 * x - 0.2173;
+y = 0.5149 * x - 0.1925;
 
 plot(x,y);
 
+legend('Data','y = 05149x - 0.1925','Location', 'southeast', 'fontsize', 15)
 
 % Make the plots beautiful
 % set( P1, 'color', '[0 0 1]', 'LineWidth', 2 );
-title('Steering gain')
-ylabel('Steering gain')
-xlabel('Vehicle velocity')
+title('Steering Gain vs. Velocity', 'fontsize', 13)
+ylabel('Steering Gain', 'fontsize', 12)
+xlabel('Vehicle Velocity [m \cdot s^{-1}]', 'fontsize', 12)
 % xlim([3 7]);
 % legend('Theta error', 'Theta','Velocity of X m \cdot s^{-1}')
 grid on
