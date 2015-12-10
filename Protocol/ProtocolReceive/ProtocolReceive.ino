69963bd4fa51da41002cdffa3fed16969be3e703
TempestSky <mgotth13@student.aau.dk>
2015-12-10 11:35:29 +0100

    Morning

--- Protocol/ProtocolReceive/ProtocolReceive.ino
+++ Protocol/ProtocolReceive/ProtocolReceive.ino
@@ -29,18 +29,3 @@ void loop() {
   }  
 }
 
-float DistanceCal(int Point1x, int Point1y, int Point2x, int Point2y, int Actx, int Acty) {
-
-  float p1x = Point1x / 1000.0; 
-  float p1y = Point1y / 1000.0;
-  float p2x = Point2x / 1000.0;
-  float p2y = Point2y / 1000.0;
-  float acx = Actx / 1000.0;
-  float acy = Acty / 1000.0;
-  
-  float temp1 = ((p2y -p1y) * acx) - ((p2x -p1x) * acy) + (p2x * p1y) - (p2y * p1x);
-  float temp2 = (float)sqrt(pow((p2y - p1y),2) + pow((p2x - p1x),2));
-
-  return temp1/temp2;
-}
-
---
+++ drivers/Newest Driver 10-12-2015/ProtocolRec.h
@@ -0,0 +1,11 @@
+#include <arduino.h>
+
+typedef struct {
+  uint8_t Destination;
+  uint8_t Length;
+  int16_t Xcor;
+  int16_t Ycor;
+  int16_t Zcor;
+}Protocol;
+
+int GetProtocol(int out[5]);
