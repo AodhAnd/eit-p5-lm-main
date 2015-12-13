4024b4d97cfc335865f36d43665c050e759cc7f2
Niels Skov Vestergaard <Niels Skov Vestergaard>
2015-12-13 16:52:20 +0100

    Changed some code and simulation for PI controllers

--- simulation/velocityModel/PImatlab/testPI.m
+++ simulation/velocityModel/PImatlab/testPI.m
@@ -1,3 +1,5 @@
+p = get(gcf, 'Position')           % These two lines makes the figuresappear the same
+set(0, 'DefaultFigurePosition', p) % place on the screen as where you left them last
 clear all
 close all;
 clc;
@@ -12,7 +14,7 @@ data(:,1) = (data(:,1)-4035)/1000;
 plot( simData(:,1), simData(:,2), 'r', 'LineWidth', 2);
 hold on;
 plot( data(:,1), data(:,2), 'b', 'LineWidth', 2);
-scatter( data(:,1), data(:,2), 20, [0 .3 0], 'o' );
+scatter( data(:,1), data(:,2), 20, [0 .5 0], 'o' );
 
 %Title and axis labels added
 title('PI-Controller Simulation and Implementation Compared')
