2ad84c1e89725192c887e73096623567fb051361
Amalie <apet13@student.aau.dk>
2015-12-14 18:27:02 +0100

    Julien picture verfification

--- simulation/SteeringSimulation/SteeringPlant/steeringStepResponse.m
+++ simulation/SteeringSimulation/SteeringPlant/steeringStepResponse.m
@@ -1,6 +1,6 @@
-%close all;
-%clear all;
-%clc;
+close all;
+clear all;
+clc;
 
 
 
@@ -8,7 +8,7 @@ v= 1.4
 Ks = 0.5149 * v - 0.1925
 offset = 1360 + 270
 
-csvwrite('sim1.csv',ScopeData);
+%csvwrite('sim1.csv',ScopeData);
 datasim = csvread('sim1.csv');
 
 Data1 = csvread('stepResponse1670.csv');
@@ -20,13 +20,13 @@ Datasim = csvread('sim1.csv');
 time   = Data1(:,1);
 angle  = Data1(:,2);
 time2  = Data2(:,1);
-angle2 = Data2(:,2);
+angle2 = Data2(:,2)-10;
 timesim  = Datasim(:,1)*1000;
 anglesim = Datasim(:,2);
 
 hold;
-P1 = scatter(time,angle,'b');
-P2 = scatter(time2,angle2,'g');
+% P1 = plot(time,angle,'b');
+P2 = plot(time2,angle2,'g');
 Psim = plot(timesim,anglesim,'LineWidth',2,'color','r');
 
 
@@ -34,10 +34,10 @@ Psim = plot(timesim,anglesim,'LineWidth',2,'color','r');
 xlim([2000 10000]);
 ylim([-50 150]);
 
-title('Step response at a PWM of 1670 m \cdot s{-1}')
+title('Step response with a 1670 ms pulse width')
 xlabel('Time [ms]')
-ylabel('Angle [Â°]')
+ylabel('Angle [°]')
 
-legend('First test', 'Second test','Simulation','Location', 'northwest' )
+legend('Measured data', 'Simulation','Location', 'southeast' )
 grid on
 %set(gca,'GridLineStyle',':', 'GridColor', 'k', 'GridAlpha', .6)
