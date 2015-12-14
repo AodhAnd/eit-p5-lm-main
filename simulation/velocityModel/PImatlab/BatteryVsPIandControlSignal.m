414c403cc6b55526b3f760cc57f872e7fc42d23f
Niels Skov Vestergaard <Niels Skov Vestergaard>
2015-12-14 18:07:14 +0100

    changed a few things in a matlab script for the veolcity controller

--- simulation/velocityModel/PImatlab/BatteryVsPIandControlSignal.m
+++ simulation/velocityModel/PImatlab/BatteryVsPIandControlSignal.m
@@ -4,17 +4,17 @@ clear all
 close all;
 clc;
 
-%csvwrite('simulationPForwardsingleStep.csv', ScopeData3);
-simData = csvread('simulationPForwardsingleStep.csv', 0, 0);
+%csvwrite('simulationPIcalculated.csv', ScopeData3);
+simData = csvread('simulationPIcalculated.csv', 0, 0);
 simData(:,1) = simData(:,1)-2
 
-data = csvread('LOG00322.csv'); % step 294 for calculated PI controller
+data = csvread('LOG00294.csv'); % step 294 for calculated PI controller
                                 % step 288 for tuned PI controller
                                 % step 299 for P controller with feed forward
                                 % step 316 (without) and 322 (width) anti-windup
 
 %data = csvread('LOG00217.csv');%, 0, 0); 18 to 22    use step 16
-data(:,1) = (data(:,1)-3980)/1000;
+data(:,1) = (data(:,1)-3970)/1000;
 %data1 = csvread('LOG00224.csv');%, 0, 0);
 %data1(:,1) = (data1(:,1)-4035)/1000;
 %data2 = csvread('LOG00225.csv');%, 0, 0);
@@ -24,10 +24,10 @@ data(:,1) = (data(:,1)-3980)/1000;
 
 %data(:,3) = data(:,3);%-5.5;
 
-%[AX, vel, bat] = plotyy( simData(:,1), simData(:,2), data(:,1), data(:,3));
-[AX, vel, bat] = plotyy( data(:,1), data(:,2), data(:,1), data(:,3));
+[AX, vel, bat] = plotyy( simData(:,1), simData(:,2), data(:,1), data(:,3));
+%AX, vel, bat] = plotyy( data(:,1), data(:,2), data(:,1), data(:,3));
 set(AX,'NextPlot','add')
-%plot(AX(1), data(:,1), data(:,2), 'Color', [0 0 1], 'LineWidth', 2);
+plot(AX(1), data(:,1), data(:,2), 'Color', [0 0 1], 'LineWidth', 2);
 pAdd = plot(AX(2), data(:,1), data(:,4) );
 set(pAdd, 'Color', [1 0 0], 'LineWidth', 2 );
 
@@ -35,24 +35,24 @@ set(pAdd, 'Color', [1 0 0], 'LineWidth', 2 );
 %pAdd = plot(AX(1), data(:,1), data(:,5) );
 %set(pAdd, 'Color', [0 .5 0], 'LineWidth', 2 );
 
-set(AX(1),'xlim',[0 3], 'ylim', [0 3], 'ycolor', [0 0 1]);
-set(vel, 'Color', [0 0 1], 'LineWidth', 2);
-set(AX(2),'xlim',[0 3], 'ylim', [1.5 8], 'ycolor', [.7 0 0]);
+set(AX(1),'xlim',[0 3], 'ylim', [0 2], 'ycolor', [0 0 1]);
+set(vel, 'Color', [0 .5 0], 'LineWidth', 2);
+set(AX(2),'xlim',[0 3], 'ylim', [0 8], 'ycolor', [.7 0 0]);
 set(bat, 'Color', [.3 0 0], 'LineWidth', 2);
 
-%hline = line([1 10], [1.4 1.4]);
-hline = line([1 10], [2 2]);
+hline = line([1 10], [1.4 1.4]);
+%hline = line([1 10], [2 2]);
 set(hline, 'Color', [.3 .3 .3], 'LineWidth', 1, 'LineStyle', '--');
 
-%legend('Simulated step response', 'Measured step response', 'Input step to 1.4 m\cdot s^{-1}', 'Battery voltage', 'Controller output', 'Location', 'southeast')
-legend('Measured step response', 'Input step to 2 m\cdot s^{-1}', 'Battery voltage', 'Controller output', 'Location', 'southeast')
+legend('Simulated step response', 'Measured step response', 'Input step to 1.4 m\cdot s^{-1}', 'Battery voltage', 'Controller output', 'Location', 'southeast')
+%legend('Measured step response', 'Input step to 2 m\cdot s^{-1}', 'Battery voltage', 'Controller output', 'Location', 'southeast')
 
-%hline = line([1 1], [0 1.4]);
-hline = line([1 1], [0 2]);
+hline = line([1 1], [0 1.4]);
+%hline = line([1 1], [0 2]);
 set(hline, 'Color', [.3 .3 .3], 'LineWidth', 1, 'LineStyle', '--');
 
-hline = line([1.129 1.129], [0 3]);
-set(hline, 'Color', [0 0 0], 'LineWidth', 1.3, 'LineStyle', ':');
+%hline = line([1.129 1.129], [0 3]);
+%set(hline, 'Color', [0 0 0], 'LineWidth', 1.3, 'LineStyle', ':');
 
 %%%%%plot( data(:,1), data(:,3), 'k', 'LineWidth', 2);
 %scatter( data(:,1), data(:,2), 100, [0 .3 0], '.' );
@@ -71,7 +71,7 @@ set(hline, 'Color', [0 0 0], 'LineWidth', 1.3, 'LineStyle', ':');
 %scatter( data3(:,1), data3(:,2), 100, [0 .3 0], '.' );
 
 %Title and axis labels added
-title('PI-Controller at 2 m \cdot s^{-1}')
+title('PI-Controller and Simulation Comparrison')
 xlabel('Time [s]')
 ylabel(AX(1), 'Linear velocity [ m \cdot s^{-1} ]')
 ylabel(AX(2), 'Voltage [ V ]')
